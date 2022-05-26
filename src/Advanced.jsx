f4e124e19e2c9f4c0ef1785648bd1e9a06ff6051
Priya-05 <56279297+Priya-05@users.noreply.github.com>
2022-05-26 12:39:36 -0500

    with styling

---
+++ src/Advanced.jsx
@@ -0,0 +1,143 @@
+//@ts-check
+"https://codesandbox.io/s/8tzm6?file=/src/examples/Advanced.js:0-3729";
+
+import React, { useState, useMemo, useRef } from "react";
+// import TinderCard from '../react-tinder-card/index'
+import TinderCard from "react-tinder-card";
+import { testItems } from "./test-items";
+
+export function RiExternalLinkLine(props) {
+  return (
+    <svg width=".75em" height=".75em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="#fff"
+        d="M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h6zm11-3v8h-2V6.413l-7.793 7.794l-1.414-1.414L17.585 5H13V3h8z"
+      ></path>
+    </svg>
+  );
+}
+
+function Advanced() {
+  const [currentIndex, setCurrentIndex] = useState(testItems.length - 1);
+  const [lastDirection, setLastDirection] = useState();
+  // used for outOfFrame closure
+  const currentIndexRef = useRef(currentIndex);
+
+  const childRefs = useMemo(
+    () =>
+      Array(testItems.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  );
+
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+
+  const canGoBack = currentIndex < testItems.length - 1;
+
+  const canSwipe = currentIndex >= 0;
+
+  // set last direction and decrease current index
+  const swiped = (direction, nameToDelete, index) => {
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
+  };
+
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+    // TODO: when quickly swipe and restore multiple times the same card,
+    // it happens multiple outOfFrame events are queued and the card disappear
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  };
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < testItems.length) {
+      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    }
+  };
+
+  // increase current index and show card
+  const goBack = async () => {
+    if (!canGoBack) return;
+    const newIndex = currentIndex + 1;
+    updateCurrentIndex(newIndex);
+    await childRefs[newIndex].current.restoreCard();
+  };
+
+  return (
+    <div>
+      <link
+        href="https://fonts.googleapis.com/css?family=Damion&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Alatsi&display=swap"
+        rel="stylesheet"
+      />
+      <h1>React Tinder Card</h1>
+      <div className="cardContainer">
+        {testItems.map(({ title, url, summary }, index) => (
+          <TinderCard
+            ref={childRefs[index]}
+            className="swipe"
+            key={title}
+            onSwipe={(dir) => swiped(dir, title, index)}
+            onCardLeftScreen={() => outOfFrame(title, index)}
+          >
+            <div className="card text-black flex flex-col gap-8">
+              <h2 className="whitespace-pre-wrap">{title}</h2>
+              <p className="whitespace-pre-wrap">{summary}</p> 
+              <a
+                className="flex items-start gap-1"
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Open
+                <span className="pt-1">
+                  <RiExternalLinkLine />
+                </span>
+              </a>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("left")}
+        >
+          Swipe left!
+        </button>
+        <button
+          style={{ backgroundColor: !canGoBack && "#c3c4d3" }}
+          onClick={() => goBack()}
+        >
+          Undo swipe!
+        </button>
+        <button
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("right")}
+        >
+          Swipe right!
+        </button>
+      </div>
+      {lastDirection ? (
+        <h2 key={lastDirection} className="infoText">
+          You swiped {lastDirection}
+        </h2>
+      ) : (
+        <h2 className="infoText">
+          Swipe a card or press a button to get Restore Card button visible!
+        </h2>
+      )}
+    </div>
+  );
+}
+
+export default Advanced;
--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,18 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import './App.css'
+import Switch from 'react-ios-switch'
 
-// https://tailwindcss.com/
-function App() {
-  const [count, setCount] = useState(0)
+import Advanced from './Advanced'
+
+function App () {
+  const [showAdvanced, setShowAdvanced] = useState(true)
 
   return (
-    <div>
-      <header>
-        <p className="text-red text-8">Hello Vite + React!</p>
-        <p className="text-green text-3 bg-blue">
-          <button
-            className="hover:bg-pink px-4 py-2 rounded"
-            type="button"
-            onClick={() => setCount(count => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='app'>
+      <Advanced />
+      <div className='row'>
+        <p style={{ color: '#fff' }}>Show advanced example</p> <Switch checked={showAdvanced} onChange={setShowAdvanced} />
+      </div>
     </div>
   )
 }
